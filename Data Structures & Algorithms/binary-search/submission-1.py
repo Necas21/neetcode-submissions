a8dc9d0988a3b7973145f507cef77a4b95bd00f1
Necas21 <mounir.hedna@yahoo.com>
2026-07-26 09:29:42 +0100

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # Pointers to start and end of list
+        start = 0
+        end = len(nums) - 1
+
+        while start <= end:
+            # Calculate mid point
+            mid = ((end - start) // 2) + start
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                start = mid + 1
+            else:
+                end = mid - 1
+        
+
+        return - 1
+        
